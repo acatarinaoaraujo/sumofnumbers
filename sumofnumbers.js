4e90f5166168f0fd9b545236fc98f4da843b5c48
acatarinaoaraujo <60526884+acatarinaoaraujo@users.noreply.github.com>
2021-02-07 22:35:58 -1000

    Finished Exercise.

---
+++ sumofnumbers.js
@@ -0,0 +1,39 @@
+const testNum = [1, 2, 3, 4];
+
+function sumFor(nums) {
+  let sum = 0;
+  for (const num of nums) {
+    sum += num;
+  }
+  return sum;
+}
+
+console.log(sumFor(testNum));
+
+function sumWhile(nums) {
+  let sum = 0;
+  let i = 0;
+  while (i < nums.length) {
+    sum += nums[i];
+    i++;
+  }
+  return sum;
+}
+
+console.log(sumWhile(testNum));
+
+function sumRecursion(nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  return nums[0] + sumRecursion(nums.slice(1, nums.length));
+}
+
+console.log(sumRecursion(testNum));
+
+function sumTheSimpleWay(nums) {
+  const sum = _.reduce(nums, function (memo, num) { return memo + num; }, 0);
+  return sum;
+}
+
+console.log(sumTheSimpleWay(testNum));
